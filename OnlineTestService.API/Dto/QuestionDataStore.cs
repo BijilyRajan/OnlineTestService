c6d81db677b4d10dbbab8bc1b05361f4c84348b7
Bijily <bijily.rajan@teranet.ca>
2022-04-19 15:25:05 +0530

    Updated questions

--- OnlineTestService.API/Dto/QuestionDataStore.cs
+++ OnlineTestService.API/Dto/QuestionDataStore.cs
@@ -11,8 +11,11 @@ namespace OnlineTestService.API
             QuestionName = "handsetCards";
             questions =  new List<Question>(){  
 
-            new Question() { title = "Question 1", content = "Which of the following is the correct ways to set a value 3.14 in a variable pi such that it cannot be modified?", choices = new List<string> { "pi = 3.14F", "const float pi", "float pi = 3.14F" } , cols = 1, rows = 1  },
-            new Question() { title = "Question 1", content = "Which is a software language", choices = new List<string> { "Agile", "C++", "Eclipse" } , cols = 1, rows = 1  }
+            new Question() { title = "Question 1", content = "Which of the following is the correct ways to set a value 3.14 in a variable pi such that it cannot be modified?", choices = new List<string> { "float pi = 3.14F", "const float pi = 3.14F", "const float pi; pi = 3.14F", "pi = 3.14F" } , cols = 1, rows = 1  },
+            new Question() { title = "Question 2", content = "Which is a software language", choices = new List<string> { "Agile", "C++", "Eclipse", "Azure" } , cols = 1, rows = 1  },
+            new Question() { title = "Question 3", content = "Which is not a feature of OOP in general definitions", choices = new List<string> { "Efficient Code", "Code reusability", "Modularity", "Redundant data" } , cols = 1, rows = 1  },
+            new Question() { title = "Question 4", content = "Which among the following is correct?", choices = new List<string> { "void test(int x=0, int y, int z=0)", "void test(int x=0, int=0)", "void test(int x, int y=0)", "void test(int x=’c, int y)" } , cols = 1, rows = 1  },
+            new Question() { title = "Question 5", content = "In C# String is a value type", choices = new List<string> { "True", "False" } , cols = 1, rows = 1  }
 
             };
         }
